d91c84cb463fd4e50fa4da4046526adc66685eb7
LeTuanAnh1409 <151327950+LeTuanAnh1409@users.noreply.github.com>
2024-09-25 22:05:43 +0700

    Update db.config.js

--- BE/config/db.config.js
+++ BE/config/db.config.js
@@ -1,4 +1,3 @@
-
 import mongoose from 'mongoose';
 
 const connectDB = async (db) => {
@@ -10,23 +9,3 @@ const connectDB = async (db) => {
   }
 };
 export default connectDB;
-
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  try {
-    const dbURI = process.env.MONGO_URI;
-    await mongoose.connect(dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected successfully!');
-  } catch (error) {
-    console.error('Database connection failed:', error);
-    process.exit(1); 
-  }
-};
-
-module.exports = connectDB;
-
